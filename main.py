0ca0dc6dd73df1da5b7bc0341921f26d8c2d7776
macuser10-6 <77914226+macuser10-6@users.noreply.github.com>
2022-10-16 06:38:35 -0400

    completed GetPrice for URL

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from requests_html import HTMLSession
 
+urls = [
+    ''
+    ''
+    ''
+]
+
+
 def getPrice(url):
     s = HTMLSession()
     r = s.get(url)
     r.html.render(sleep=1)
-    
+
     product = {
-		'title': r.html.xpath('//*[@id="productTitle"]', first=True).text,
-	    'price': r.html.xpath('//*[@id="priceblocck_ourprice"]', first=True).text
-	}
+        'title': r.html.xpath('//*[@id="productTitle"]', first=True).text,
+        'price': r.html.xpath('//*[@id="priceblocck_ourprice"]', first=True).text
+    }
     print(product)
     return product
+
+
+tvprices = [getPrice(url) for url in urls]
+print(len(tvprices))
