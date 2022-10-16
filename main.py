ef545f38651c1efeaef2f5e56f17267af4ebc5ac
macuser10-6 <77914226+macuser10-6@users.noreply.github.com>
2022-10-16 07:29:54 -0400

    updated main added api

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-from requests_html import HTMLSession
+from fastapi import FastAPI
 
-urls = [
-    ''
-    ''
-    ''
-]
+import requests
+from bs4 import BeautifulSoup
 
-
-def getPrice(url):
-    s = HTMLSession()
-    r = s.get(url)
-    r.html.render(sleep=1)
-
-    product = {
-        'title': r.html.xpath('//*[@id="productTitle"]', first=True).text,
-        'price': r.html.xpath('//*[@id="priceblocck_ourprice"]', first=True).text
-    }
-    print(product)
-    return product
+app = FastAPI()
 
 
-tvprices = [getPrice(url) for url in urls]
-print(len(tvprices))
+@app.get("/{asin}")
+async def get_data(asin: str):
+    session = requests.Session()
+    session.headers.update({
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
+    })
+    resp = session.get(f"https://amazon.co.uk/dp/{asin}")
+    if resp.status_code != 200:
+        return {"error": f"bad status code {resp.status_code}"}
+    soup = BeautifulSoup(resp.text, "html.parser")
+    try:
+        data = {
+            "asin": asin,
+            "name": soup.select_one("h1#title").text.strip(),
+            "price": soup.select_one("span.a-offscreen").text,
+        }
+        return {"results": data}
+    except KeyError:
+        return {"error": "Unable to parse page"}
